65849341451664e211805e911da81a16bb757e97
ZooLee <zzllee92@gmail.com>
2017-02-13 15:45:21 +0100

    more functionality

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (request, response) => {  
-  response.send('Hello from Express!')
+  response.send('Hello from Express!');
+  console.log('I said HEY! WHATS GOING ON?');
 })
 
 app.listen(port, (err) => {  
